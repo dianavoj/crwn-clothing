ab83786776e67f1f5fdd99fec5bc22e67e201282
dianavoj <diana.vojevodina@gmail.com>
2023-06-18 13:53:38 +0100

    fixed cart-dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -17,9 +17,9 @@ const CartDropdown = () => {
     return (
         <CartDropdownContainer>
             <CartItems>
-                {cartItems.length ? (cartItems.map(item => (
-                    <CartItem key={item.id} cartItem={item} />
-                    ))) : (
+                {cartItems.length ? (cartItems.map((item) => 
+                    <CartItem key={item.id} cartItem={item} />)
+                    ) : (
                         <EmptyMessage>Your cart is empty</EmptyMessage>
                     )}
             </CartItems>
