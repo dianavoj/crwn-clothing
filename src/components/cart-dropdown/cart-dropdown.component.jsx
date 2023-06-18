5901c0980e9108d96df0c67ed92a31927a67d500
dianavoj <diana.vojevodina@gmail.com>
2023-06-18 14:01:07 +0100

    fixed cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -21,25 +21,14 @@ const CartDropdown = () => {
 
         <CartDropdownContainer>
             <CartItems>
-                {cartItems.length ? (cartItems.map((item) => 
-                    <CartItem key={item.id} cartItem={item} />)
+                {cartItems.length ? (
+                    cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
                     ) : (
                         <EmptyMessage>Your cart is empty</EmptyMessage>
                     )}
             </CartItems>
             <Button onClick={goToCheckoutHandler}>go to checkout</Button>
         </CartDropdownContainer>
-
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
-            </div>
-
-            <Button onClick={goToCheckoutHandler}>go to checkout</Button>
-
-            <Button>go to checkout</Button>
-        </div>
-
     );
 };
 
